270db783e743717e8a18e7449c03de43e22317cb
Giveth2024 <givendarian@gmail.com>
2025-09-30 10:55:59 +0300

    Removed the movie table

--- functions/omdbFetch.js
+++ functions/omdbFetch.js
@@ -2,26 +2,13 @@ const axios = require("axios");
 
 const OMDB_API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
 
-const { saveMoviesToDB, getMoviesFromDB } = require("../functions/movie");
-
 async function searchOMDb(query, type = "movie", page = 1) {
-  // 1️⃣ Check DB first
-  const cachedMovies = await getMoviesFromDB(query, type, page);
-  if (cachedMovies.length > 0) {
-    console.log(`[Cache Hit] Returning movies from DB for query: ${query}`);
-    return { Search: cachedMovies };
-  }
-
-  // 2️⃣ If not found, fetch from OMDb
+  // Fetch directly from OMDb
   const res = await axios.get("http://www.omdbapi.com/", {
     params: { apikey: OMDB_API_KEY, s: query, type, page },
   });
 
-  if (res.data.Search) {
-    await saveMoviesToDB(res.data.Search);
-  }
-
-  return res.data;
+  return res.data; // send OMDb results straight to the frontend
 }
 
 async function getDetails(imdbID) {
